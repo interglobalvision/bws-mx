a5c93f071bebdec72b4adf77b88453dd0381b2ff
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-08-22 17:40:31 -0500

    add CV post type. set post type menu icons

--- lib/post-types.php
+++ lib/post-types.php
@@ -6,7 +6,16 @@ function add_menu_icons_styles(){
 
 <style>
 #menu-posts-work .dashicons-admin-post:before {
-    content: '\f319';
+    content: '\f128';
+}
+#menu-posts-event .dashicons-admin-post:before {
+    content: '\f509';
+}
+#menu-posts-artist .dashicons-admin-post:before {
+    content: '\f483';
+}
+#menu-posts-cv .dashicons-admin-post:before {
+    content: '\f123';
 }
 </style>
 
@@ -150,4 +159,49 @@ function register_cpt_artist() {
   );
 
   register_post_type( 'artist', $args );
+}
+
+// CV
+
+add_action( 'init', 'register_cpt_cv' );
+
+function register_cpt_cv() {
+
+  $labels = array(
+    'name' => _x( 'CVs', 'cv' ),
+    'singular_name' => _x( 'CV', 'cv' ),
+    'add_new' => _x( 'Add New', 'cv' ),
+    'add_new_item' => _x( 'Add New CV', 'cv' ),
+    'edit_item' => _x( 'Edit CV', 'cv' ),
+    'new_item' => _x( 'New CV', 'cv' ),
+    'view_item' => _x( 'View CV', 'cv' ),
+    'search_items' => _x( 'Search CVs', 'cv' ),
+    'not_found' => _x( 'No cvs found', 'cv' ),
+    'not_found_in_trash' => _x( 'No cvs found in Trash', 'cv' ),
+    'parent_item_colon' => _x( 'Parent CV:', 'cv' ),
+    'menu_name' => _x( 'CVs', 'cv' ),
+  );
+
+  $args = array(
+    'labels' => $labels,
+    'hierarchical' => false,
+
+    'supports' => array( 'title', 'editor', 'thumbnail' ),
+
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'menu_position' => 5,
+
+    'show_in_nav_menus' => true,
+    'publicly_queryable' => true,
+    'exclude_from_search' => false,
+    'has_archive' => true,
+    'query_var' => true,
+    'can_export' => true,
+    'rewrite' => true,
+    'capability_type' => 'post'
+  );
+
+  register_post_type( 'cv', $args );
 }
