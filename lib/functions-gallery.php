7bf3189481a233a88ee8875692e96758806dd781
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-08-25 19:29:26 -0500

    add captions to slides

--- lib/functions-gallery.php
+++ lib/functions-gallery.php
@@ -1,6 +1,6 @@
 <?php
-// Generate image gallery sliders
-function generate_gallery($post_id) {
+// Render image gallery sliders
+function render_gallery($post_id) {
   $post_type = get_post_type($post_id);
   $works_images = get_post_meta($post_id, '_igv_documentation_works', true);
   $install_images = get_post_meta($post_id, '_igv_documentation_install', true);
@@ -11,12 +11,12 @@ function generate_gallery($post_id) {
 <?php
   if (!empty($works_images)) {
     $active = $post_type == 'work' ? true : false;
-    build_slider($works_images, $active, 'works');
+    build_slider($works_images, $active, 'works', $post_type);
   }
 
   if (!empty($install_images)) {
     $active = $post_type == 'event' ? true : false;
-    build_slider($install_images, $active, 'install');
+    build_slider($install_images, $active, 'install', $post_type);
   }
 
 ?>
@@ -55,7 +55,7 @@ function generate_gallery($post_id) {
   }
 ?>
       </div>
-      <div class="grid-item item-m-4 desktop-only js-slide-caption"></div>
+      <div class="grid-item item-m-4 desktop-only slider-controls-caption text-align-center"></div>
       <div class="grid-item item-s-6 item-m-4 text-align-right">
 <?php
     if (!empty($works_images)) {
@@ -86,17 +86,26 @@ function slider_buttons($active, $type) {
 }
 
 // Make Swiper slider
-function build_slider($images, $active, $type) {
+function build_slider($images, $active, $type, $post_type) {
 ?>
 <div id="slider-holder-<?php echo $type; ?>" class="grid-row slider-holder<?php echo $active ? ' active' : '';?>">
-  <div class="swiper-container swiper-<?php echo $type; ?> grid-item item-s-10">
+  <div id="swiper-<?php echo $type; ?>" class="swiper-container grid-item item-s-10">
     <div class="swiper-wrapper align-items-center margin-bottom-mid">
 <?php
   foreach($images as $image_id => $image) {
+    // Get captions by post type in both langs
+    $caption_en = get_post_meta($image_id, $post_type . '_caption_en', true);
+    $caption_es = get_post_meta($image_id, $post_type . '_caption_es', true);
 ?>
       <div class="swiper-slide text-align-center u-pointer">
         <?php echo wp_get_attachment_image($image_id, 'gallery'); ?>
-        <div class="mobile-only">Caption</div>
+<?php
+    if (!empty($caption_en) || !empty($caption_es)) {
+?>
+        <div class="slide-caption mobile-only"><? _e('[:en]' . $caption_en . '[:es]' . $caption_es . '[:]'); ?></div>
+<?php
+    }
+?>
       </div>
 <?php
   }
