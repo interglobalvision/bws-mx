31b93c1df8b8bdda2bdd38f72c65f238e87f7ee9
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-08-30 14:16:13 -0500

    scaffold page template

---
+++ page.php
@@ -0,0 +1,34 @@
+<?php
+get_header();
+?>
+
+<main id="main-content">
+    <div class="container">
+      <div class="grid-row">
+
+<?php
+if (have_posts()) {
+  while (have_posts()) {
+    the_post();
+?>
+
+      <article <?php post_class('grid-item item-s-12'); ?> id="post-<?php the_ID(); ?>">
+
+        <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+
+        <?php the_content(); ?>
+
+      </article>
+
+<?php
+  }
+}
+?>
+
+    </div>
+  </div>
+</main>
+
+<?php
+get_footer();
+?>
