cce90d18d4331c039d3a323376405b0baee048dd
Patrick Best <patrickbest@patrickbest.com>
2017-08-30 20:35:54 +0100

    set breadcrumb links

--- single-cv.php
+++ single-cv.php
@@ -11,14 +11,14 @@ if (have_posts()) {
   while (have_posts()) {
     the_post();
 
-    $artists = igv_get_post_artists($post->ID);
+    $artists = wp_get_post_terms($post->ID, 'artist');
 
-    if (!empty($artists)) {
+    if (count($artists)) {
 ?>
         <article <?php post_class('grid-item item-s-12'); ?> id="post-<?php the_ID(); ?>">
 
-          <h3 class="margin-bottom-small">Artist / CV</h3>
-          <h1 class="margin-bottom-basic"><?php echo $artists; ?></h1>
+          <h3 class="margin-bottom-small"><a href="<?php echo get_term_link($artists[0]); ?>">Artist</a> / CV</h3>
+          <h1 class="margin-bottom-basic"><?php echo $artists[0]->name; ?></h1>
 
           <?php the_content(); ?>
 
