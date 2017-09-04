709d6b5df0f8ca2f087e58258f17eeda2cfa38ee
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-09-04 17:45:28 -0500

    add gallery arrows. fix caption style

--- lib/functions-gallery.php
+++ lib/functions-gallery.php
@@ -26,7 +26,7 @@ function render_gallery($post_id) {
   <div id="slider-controls" class="container">
 
     <div class="grid-row justify-between align-items-start">
-      <div class="grid-item item-s-6 item-m-4">
+      <div class="grid-item item-s-6 item-m-4 font-size-small font-bold">
 <?php
   if ($post_type == 'work') {
     if (!empty($works_images)) {
@@ -55,7 +55,7 @@ function render_gallery($post_id) {
   }
 ?>
       </div>
-      <div class="grid-item item-m-4 desktop-only slider-controls-caption text-align-center"></div>
+      <div class="grid-item item-m-4 slider-controls-caption text-align-center font-serif font-size-tiny"></div>
       <div class="grid-item item-s-6 item-m-4 text-align-right">
 <?php
     if (!empty($works_images)) {
@@ -79,8 +79,8 @@ function render_gallery($post_id) {
 function slider_buttons($active, $type) {
 ?>
 <div id="slider-buttons-<?php echo $type; ?>" class="slider-buttons<?php echo $active ? ' active' : '';?>">
-  <div class="u-inline-block slider-button slider-prev-<?php echo $type; ?>"><</div>
-  <div class="u-inline-block slider-button slider-next-<?php echo $type; ?>">></div>
+  <div class="u-inline-block slider-button slider-prev-<?php echo $type; ?>"><?php echo url_get_contents(get_template_directory_uri() . '/dist/img/arrow-left.svg'); ?></div>
+  <div class="u-inline-block slider-button slider-next-<?php echo $type; ?>"><?php echo url_get_contents(get_template_directory_uri() . '/dist/img/arrow-right.svg'); ?></div>
 </div>
 <?php
 }
@@ -89,7 +89,7 @@ function slider_buttons($active, $type) {
 function build_slider($images, $active, $type, $post_type) {
 ?>
 <div id="slider-holder-<?php echo $type; ?>" class="grid-row slider-holder<?php echo $active ? ' active' : '';?>">
-  <div id="swiper-<?php echo $type; ?>" class="swiper-container grid-item item-s-10">
+  <div id="swiper-<?php echo $type; ?>" class="swiper-container grid-item item-s-9">
     <div class="swiper-wrapper align-items-center margin-bottom-mid">
 <?php
   foreach($images as $image_id => $image) {
@@ -102,7 +102,7 @@ function build_slider($images, $active, $type, $post_type) {
 <?php
     if (!empty($caption_en) || !empty($caption_es)) {
 ?>
-        <div class="slide-caption mobile-only"><? _e('[:en]' . $caption_en . '[:es]' . $caption_es . '[:]'); ?></div>
+        <div class="slide-caption text-align-center font-serif font-size-tiny"><? _e('[:en]' . $caption_en . '[:es]' . $caption_es . '[:]'); ?></div>
 <?php
     }
 ?>
