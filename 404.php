18b7c5384ec75f487745449c7290671c7970cf20
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-09-12 09:00:18 -0700

    add spanish text

--- 404.php
+++ 404.php
@@ -7,7 +7,7 @@ get_header();
     <div class="grid-row">
       <div class="grid-item item-s-12">
         <h1 class="font-serif font-italic font-size-large margin-bottom-small">404</h1>
-        <p class="font-sans">Not found</p>
+        <p class="font-sans"><?php _e('[:en]Not found[:es]No encontrada[:]'); ?></p>
       </div>
     </div>
   </section>
